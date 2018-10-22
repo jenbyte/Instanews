68c708855a1bd1c52d53b6e9016d67914fc1f1f3
Jennifer <jen.yiu.90@gmail.com>
2018-10-22 16:49:06 -0700

    pictures and abstract append, flex wrap works, changed select bar layout

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
+  console.log("hi how are you?");
+  $(".loading").hide();
   $("#dropdown-menu").on("change", function() {
     const selected = $(this).val();
-    if (selected !== "") {
+    if (selected !== " ") {
     }
-  });
-
-  let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  url +=
-    "?" +
-    $.param({
-      "api-key": "8397de144fcd4e17a0eadad84f2033cb"
-    });
-  $.ajax({
-    url: url,
-    method: "GET"
-  }) // eo $.ajax
-    .done(function(data) {
-      $(".stories").empty();
-      $.each(data.results, function(key, value) {
-        $(".stories").append(
-          `<img src="${value.url}">` + `<p>${value.abstract}</p>`
-        );
-        console.log(data.results);
+    let url = `https://api.nytimes.com/svc/topstories/v2/${selected}.json`;
+    url +=
+      "?" +
+      $.param({
+        "api-key": "8397de144fcd4e17a0eadad84f2033cb"
       });
-    }) // eo .done
-    .fail(function(err) {
-      throw err;
-    }); // eo .fail
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+      .done(function(data) {
+        $(".loading").show();
+        $(".stories").empty();
+        $.each(data.results.slice(0, 12), function(key, value) {
+          console.log(value.multimedia[4].url);
+          $(".stories").append(
+            `<a href=${value.url}><li><div style="background-image: url(${
+              value.multimedia[4].url
+            })">` + `<p>${value.abstract}</p></div></li></a>`
+          );
+        });
+      }) // eo .done
+      .fail(function(err) {
+        throw err;
+      }) // eo .fail
+      .always(function() {
+        $(".loading").hide();
+      });
+  }); // eo.on
 }); // end of DOC

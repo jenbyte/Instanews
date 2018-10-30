b4f82db3882722c8779af0f9fb9e93148143e76e
Jennifer <jen.yiu.90@gmail.com>
2018-10-30 14:55:24 -0700

    change .each to for loops; added object destructures; fix centering of main page with flexbox

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-  console.log("👻 Boo!");
-  $(".loading").hide();
+  console.log("hey");
+
+  const $loader = $(".loading");
+  const $stories = $(".stories");
+  $loader.hide();
   $("#dropdown-menu").on("change", function() {
     const selected = $(this).val();
-    if (selected !== " ") {
+    if (!selected.length) {
+      // (selected !== " ") {
+      return;
     }
-    $(".loading").show();
+    $stories.empty();
+    $loader.show();
+    // get stories from the API
+    getStories(selected);
+  }); // end on change
+
+  function getStories(selected) {
     $("header").addClass("header-active");
     let url = `https://api.nytimes.com/svc/topstories/v2/${selected}.json`;
     url +=
@@ -18,27 +29,36 @@ $(document).ready(function() {
       method: "GET"
     })
       .done(function(data) {
-        $(".stories").empty();
-        console.log(data);
-        $.each(data.results.slice(0, 12), function(key, value) {
-          $(".stories").append(
-            `<li><a href=${value.url}><div style="background-image: url(${
-              value.multimedia[4].url
-            })">` + `<p>${value.abstract}</p></div></a></li>`
-          );
-          $('footer').append(<'p class="copyright">&copy; Copyright 2019 INSTANEWS</p>');
+        $stories.empty();
+        // console.log(data);
+        let filterArticle = data.results.filter(function(value) {
+          return value.multimedia.length;
         });
+        const sliceArticle = filterArticle.slice(0, 12);
+        // $.each(data.results.slice(0, 12), function(key, value) {
+        //   $stories.append(
+        //     `<li><a href=${value.url}><div style="background-image: url(${
+        //       value.multimedia[4].url
+        //     })">` + `<p>${value.abstract}</p></div></a></li>`
+        //   );
+        // });
+        for (let value of sliceArticle) {
+          const { url, multimedia, abstract } = value;
+          $stories.append(
+            `<li><a href=${url}><div style="background-image: url(${
+              multimedia[4].url
+            })">` + `<p>${abstract}</p></div></a></li>`
+          );
+        }
       }) // eo .done
       .fail(function(err) {
-        $(".stories").empty(err);
-
+        console.log("fail function is working");
+        $stories.empty();
         throw err;
-        $(".stories").empty();
-        $(".stories").append("Sorry there was an error. Please try again.");
-        console.log(err);
+        // $stories.append("Sorry there was an error. Please try again.");
       }) // eo .fail
       .always(function() {
-        $(".loading").hide();
+        $loader.hide();
       });
-  }); // eo.on
+  } // eo getStories
 }); // end of DOC
